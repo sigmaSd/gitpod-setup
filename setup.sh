08032c8cab6d2129e425df47e375e2567fc20c4f
Nbiba Bedis <bedisnbiba@gmail.com>
2022-05-31 16:31:45 +0100

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+apt update -y
+apt upgrade -y
+wget https://github.com/rui314/mold/releases/download/v1.2.1/mold-1.2.1-x86_64-linux.tar.gz
+tar -xvzf mold-1.2.1-x86_64-linux.tar.gz
+mv mold-1.2.1-x86_64-linux mold
+mkdir .cargo
+echo '[target.x86_64-unknown-linux-gnu]
+linker = "clang"
+rustflags = ["-C", "link-arg=-fuse-ld=/workspace/gitpod/mold"]' >.cargo/config.toml
